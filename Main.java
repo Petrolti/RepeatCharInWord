fa286020c13c8751cb036a88b6d14c66aa9ddbb0
Petrolti <20900770+Petrolti@users.noreply.github.com>
2019-02-21 15:30:36 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package test;
+
+import test.MVC.Command;
+
+/**
+* Created Petrolti, 2019
+* Build programm calculate characters from patern Model-View-Controler
+* This class runnable
+*/
+
+public class Main {
+    public static void main(String[] args){
+        new Command();
+    }
+}
